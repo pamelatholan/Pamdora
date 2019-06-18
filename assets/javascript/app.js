b04e30b44d8868d42cfb72e4d5b756048293591b
Gayah Karmo <gayahkarmo@gmail.com>
2019-06-17 20:37:13 -0400

    added upcoming events to page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
       "https://newsapi.org/v2/everything?q=" +
       artistName +
       "&apiKey=787c8e2866844a7c94f18cfad8f6bc06&tot&pageSize=4";
-      var queryURL2 = "https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?...&keywords=" + artistName + "&app_key=txM6TBSz6KpmbjwL";
+      var queryURL2 = "https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?keywords=" + artistName + "&app_key=txM6TBSz6KpmbjwL";
       var queryURL3 = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=" + artistName;
 
       $.ajax({
@@ -69,8 +69,18 @@ $(document).ready(function() {
         url: queryURL2,
         method: "GET"
       }).then(function(response){
-        console.log(response);
-        var results = response.events
+        response = JSON.parse(response);
+        var results = response.events.event;
+        for (var i = 0; i < results.length; i++){
+          var resultsDiv = $("<div>");
+          
+          var events = $("<p>").text(results[i].description);
+
+          resultsDiv.append(events);
+          $("#concerts").append(resultsDiv);
+
+        }
+        // var results = response.events
         // console.log(response.events.event[0].description);
     
       })
