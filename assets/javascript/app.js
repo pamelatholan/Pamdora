1f3c9240120bf722dab333eac36ec16a2be26935
Gayah Karmo <gayahkarmo@gmail.com>
2019-06-18 22:29:59 -0400

    updated concerts format

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,7 +23,6 @@ $(document).ready(function() {
     $("#articles").show();
     $("#tracks").show();
     $("#concerts").show();
-  
 
     // This will empty all the previous search results
     $("#articles").empty();
@@ -31,98 +30,117 @@ $(document).ready(function() {
     $("#tracks").empty();
     $("#concerts").empty();
 
-
     // This line grabs the input from the text box
 
-    var artistName = $("#search").val().trim();
+    var artistName = $("#search")
+      .val()
+      .trim();
 
     database.ref().push({
       newArtist: artistName
     });
 
-
     var queryURL =
       "https://newsapi.org/v2/everything?q=" +
       artistName +
       "&apiKey=787c8e2866844a7c94f18cfad8f6bc06&tot&pageSize=4";
-      var queryURL2 = "https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?keywords=" + artistName + "&app_key=txM6TBSz6KpmbjwL";
-      var queryURL3 = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=" + artistName;
+    var queryURL2 =
+      "https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?keywords=" +
+      artistName +
+      "&app_key=txM6TBSz6KpmbjwL";
+    var queryURL3 =
+      "https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=" +
+      artistName;
 
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function (response){
-       console.log(response);
-       var results = response.articles;
-       for(var i = 0; i < results.length; i++) {
-           var resultsDiv = $('<div>')
-           var p = $('<p>').text(results[i].description)
-           var myBreak = $('<br>')
-           var url = results[i].url
-           var linkTag = $('<a>').attr('href', url).text(" " + 'More Info')
-           var picUrl = results[i].urlToImage
-           var pic = $('<img>').attr( { src:picUrl, height:"200px", width: "300px" } )
-           var header = $('<h1>')
-           header.addClass("articles")
-           header.text("News Articles")
-           resultsDiv.append(p)
-           resultsDiv.prepend(linkTag)
-           resultsDiv.prepend(pic)
-           $('#articles').append(resultsDiv)
-          //  $('#events').append(pic)
-       }
-        $('#articles').prepend(header)
-       $("#artistInfo").text(artistName.toUpperCase());
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      var results = response.articles;
+      for (var i = 0; i < results.length; i++) {
+        var resultsDiv = $("<div>");
+        var p = $("<p>").text(results[i].description);
+        var myBreak = $("<br>");
+        var url = results[i].url;
+        var linkTag = $("<a>")
+          .attr("href", url)
+          .text(" " + "More Info");
+        var picUrl = results[i].urlToImage;
+        var pic = $("<img>").attr({
+          src: picUrl,
+          height: "200px",
+          width: "300px"
+        });
+        resultsDiv.append(p);
+        resultsDiv.prepend(linkTag);
+        resultsDiv.prepend(pic);
+        $("#articles").append(resultsDiv);
+        //  $('#events').append(pic)
+      }
+      var header = $("<h1>");
+      header.addClass("articles");
+      header.text("News Articles");
+      $("#articles").prepend(header);
+      $("#artistInfo").text(artistName.toUpperCase());
+    });
 
-      })
-    
-    
-      $.ajax({
-        url: queryURL2,
-        method: "GET"
-      }).then(function(response){
-        response = JSON.parse(response);
-        var results = response.events.event;
-        for (var i = 0; i < results.length; i++){
-          var resultsDiv = $("<div>");
-          
-          var events = $("<p>").text(results[i].description);
+    $.ajax({
+      url: queryURL2,
+      method: "GET"
+    }).then(function(response) {
+      response = JSON.parse(response);
+      var results = response.events.event;
+      for (var i = 0; i < results.length; i++) {
+        var resultsDiv = $("<div>");
+        var newLine = $("<br>");
+        var events = $("<p>").text(results[i].title);
+        var start = $("<p>").text(results[i].start_time);
+        var place = $("<p>").text(
+          results[i].venue_name +
+            ", " +
+            results[i].venue_address +
+            " " +
+            results[i].city_name +
+            ", " +
+            results[i].region_name
+        );
+        var url = results[i].url;
+        var getTickets = $("<a>")
+          .attr("href", url)
+          .text(" " + "Get Tickets Now!");
 
-          resultsDiv.append(events);
-          $("#concerts").append(resultsDiv);
+        resultsDiv.append(events);
+        resultsDiv.append(start);
+        resultsDiv.append(place);
+        resultsDiv.append(getTickets);
+        resultsDiv.append(newLine);
 
-        }
-        var header = $('<h1>')
-        header.addClass("tracks")
-        header.text("Upcoming Events")
-        $("#concerts").prepend(header);
-        // var results = response.events
-        // console.log(response.events.event[0].description);
-    
-      })
-      
-    
-      $.ajax({
-        url:queryURL3,
-        method: "GET"
-      }).then(function(response){
-        console.log(response);
-        var results = response.data;
-        for (var i = 0; i < 5; i++) {
-          var resultsDiv = $("<div>");
-    
-          var name = $("<p>").text(results[i].title);
-          resultsDiv.append(name);
-          $("#tracks").append(resultsDiv);
-    
-    
-        }
-        var header = $('<h1>')
-        header.addClass("tracks")
-        header.text("Top 5 Tracks")
-        $("#tracks").prepend(header);
+        $("#concerts").append(resultsDiv);
+      }
+      var header = $("<h1>");
+      header.addClass("tracks");
+      header.text("Upcoming Events");
+      $("#concerts").prepend(header);
+    });
 
+    $.ajax({
+      url: queryURL3,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      var results = response.data;
+      for (var i = 0; i < 5; i++) {
+        var resultsDiv = $("<div>");
 
-      })
+        var name = $("<p>").text(results[i].title);
+        resultsDiv.append(name);
+        $("#tracks").append(resultsDiv);
+      }
+      var header = $("<h1>");
+      header.addClass("tracks");
+      header.text("Top 5 Tracks");
+      $("#tracks").prepend(header);
+    });
   });
 });
